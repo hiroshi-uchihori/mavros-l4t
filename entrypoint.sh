59e3cb95164b1bb6125c8800d307d8630e929a97
hiroshi-uchihori <hiromx5@nifty.com>
2025-12-14 11:12:35 +0900

    Latest used in the sea trial in December

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ set -e
 # ROS 2 環境をソース
 source /opt/ros/humble/setup.bash
 
+# IP アドレスを環境変数から取得。指定がなければデフォルト値
+GCS_IP=${GCS_IP:-192.168.11.9}
+
 # MAVROS 起動
 ros2 run mavros mavros_node --ros-args \
    -p fcu_url:="/dev/ttyACM0:57600" \
-   -p gcs_url:="udp://0.0.0.0:14551@192.168.2.111:14550"
+   -p gcs_url:="udp://0.0.0.0:14551@${GCS_IP}:14550"
